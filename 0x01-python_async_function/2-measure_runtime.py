caedfa157cdef640bb3843a443e50eb7f2d44777
boladedennis <dennisb322@gmail.com>
2023-07-10 14:11:43 -0700

    async function

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,25 +1,17 @@
 #!/usr/bin/env python3
-""" measure the run time"""
+'''Task 2's module.
+'''
 import asyncio
 import time
-import random
-from typing import List
 
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n: int = 0, max_delay: int = 10) -> float:
-    """
-    Args:
-    n and max_delays
-
-    Returns Total_time/n
-    """
-    first_time = time.perf_counter()
+def measure_time(n: int, max_delay: int) -> float:
+    '''Computes the average runtime of wait_n.
+    '''
+    start_time = time.time()
     asyncio.run(wait_n(n, max_delay))
-    elapsed = time.perf_counter() - first_time
-    total_time = elapsed / n
-
-    return total_time
+    return (time.time() - start_time) / n
 
--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python3
-""" regular function """
+'''Task 3's module.
+'''
 import asyncio
-import time
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int = 0) -> asyncio.Task:
-    """
-    args: max_delay
-    return: asyncio Task
-    """
-    task = asyncio.create_task(wait_random(max_delay))
-
-    return task
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    '''Creates an asynchronous task for wait_random.
+    '''
+    return asyncio.create_task(wait_random(max_delay))
 
